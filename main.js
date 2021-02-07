d551a7db82090a0c97d920de090e635ab6f0439c
Nick Khitrov <deliriumbred@yandex.ru>
2021-02-07 12:56:29 +0300

    Make a lesson 3

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
+'use strict';
+
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg',
+            alt: 'green socks',
+            link: 'https://yandex.ru',
         }
-    }
-})
+    },
+});
