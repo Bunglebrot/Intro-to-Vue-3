6be6967d6036c683effbb2418496f8ed832c58da
Nick Khitrov <deliriumbred@yandex.ru>
2021-02-07 18:15:50 +0300

    Make a solution of a fifth lesson

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
+'use strict';
+
 const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+              { id: 2234, color: 'green', },
+              { id: 2235, color: 'blue', },
+            ],
+            productSize: [37, 40, 41, 42, 43, 44, 45],
         }
-    }
-})
+    },
+});
