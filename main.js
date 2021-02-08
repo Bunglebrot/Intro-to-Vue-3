894e74b0a9f198d178d5fc63e30270486c556307
Nick Khitrov <deliriumbred@yandex.ru>
2021-02-08 11:56:20 +0300

    Make a solution of a 8 lesson

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
+'use strict';
+
 const app = Vue.createApp({
     data() {
         return {
             cart:0,
             product: 'Socks',
             brand: 'Vue Mastery',
+            onSale: true,
             image: './assets/images/socks_blue.jpg',
-            inStock: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 0 },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 50 },
+            ],
+            selectedVariant: 0,
         }
     },
     methods: {
@@ -19,6 +22,23 @@ const app = Vue.createApp({
         },
         updateImage(variantImage) {
             this.image = variantImage
-        }
-    }
-})
+        },
+        updateVariant(index) {
+            this.selectedVariant = index;
+        },
+    },
+    computed: {
+      title() {
+        return this.brand + ' ' + this.product;
+      },
+      image() {
+        return this.variants[this.selectedVariant].image;
+      },
+      inStock() {
+        return this.variants[this.selectedVariant].quantity;
+      },
+      isProductSale() {
+        if (this.onSale) return this.brand + this.product + ' is on Sale';
+      }
+    },
+});
